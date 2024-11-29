e0c6015a7ab08121a48d00d0ea0d2903df08e1d3
aabclara <claram.miguelc03@gmail.com>
2024-11-29 18:31:19 -0300

    Exercícios Interação e Negociação PDF

---
+++ EXERCICIOS/ComandosGuia02.sql
@@ -0,0 +1,9 @@
+UPDATE TbCidade SET id=5 WHERE id=3;
+select * from TbCidade;
+
+UPDATE TbCidade SET nome='Fabriciano' WHERE id=2;
+select * from TbCidade;
+
+DELETE FROM TbCidade WHERE id=1;
+select * from TbPessoa;
+
